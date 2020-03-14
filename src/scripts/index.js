acaaaacd46bd132f37226925cb801378690ad5bb
Taylor Radtke <taylorradtke@gmail.com>
2020-03-14 17:50:59 -0400

    navabr

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,13 @@
 import 'normalize.css';
 import '../styles/index.scss';
+
+// Navbar Scroll
+window.addEventListener('scroll', () => {
+  const scrollTop = Math.round(window.scrollY);
+  const topnav = document.querySelector('.topnav');
+  if (scrollTop > 49) {
+    topnav.classList.add('topnav-fixed');
+  } else {
+    topnav.classList.remove('topnav-fixed');
+  }
+});
