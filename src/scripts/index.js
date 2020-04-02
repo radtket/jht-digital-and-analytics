d0e62db41d640f0d0280449f32fb3728d17648fa
Taylor Radtke <Radtke_Taylor@BAH.com>
2020-04-01 20:05:36 -0400

    test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
 import "normalize.css";
 import "../styles/index.scss";
 
-// Navbar Scroll
-window.addEventListener("scroll", () => {
-  const scrollTop = Math.round(window.scrollY);
-  const topnav = document.querySelector(".topnav");
-  if (scrollTop > 49) {
+const pageWrapper = document.querySelector(".ms-core-overlay");
+const topnav = document.querySelector(".topnav");
+const topnavHeight = topnav.offsetHeight;
+
+pageWrapper.addEventListener("scroll", () => {
+  const scrollTop = Math.round(pageWrapper.scrollTop);
+  if (scrollTop > topnavHeight) {
     topnav.classList.add("topnav-fixed");
   } else {
     topnav.classList.remove("topnav-fixed");
@@ -27,11 +29,10 @@ const makeNavLinksSmooth = () => {
 const spyScrolling = () => {
   const sections = document.querySelectorAll(".page-section");
 
-  window.onscroll = () => {
-    const scrollPos =
-      document.documentElement.scrollTop || document.body.scrollTop;
+  pageWrapper.onscroll = () => {
+    const scrollTop = Math.round(pageWrapper.scrollTop + topnavHeight);
     Object.values(sections).forEach(({ offsetTop, id }) => {
-      if (offsetTop <= scrollPos) {
+      if (offsetTop <= scrollTop) {
         document.querySelector(".active").classList.remove("active");
         document.querySelector(`a[href*=${id}]`).classList.add("active");
       }
@@ -42,17 +43,6 @@ const spyScrolling = () => {
 makeNavLinksSmooth();
 spyScrolling();
 
-let intervalId = 0; // Needed to cancel the scrolling when we're at the top of the page
-
-document.querySelector(".scroll").addEventListener("click", () => {
-  intervalId = setInterval(() => {
-    if (window.pageYOffset === 0) {
-      clearInterval(intervalId);
-    }
-    window.scroll(0, window.pageYOffset - 50);
-  }, 16.66);
-});
-
 // Modal
 const modalTriggers = document.querySelectorAll(`[data-modal-for]`);
 
@@ -61,12 +51,12 @@ Object.values(modalTriggers).forEach(trigger => {
   trigger.addEventListener("click", () => {
     if (trigger.hasAttribute("open")) {
       modal.showModal();
-      document.querySelector("body").classList.add("scroll-lock");
+      document.querySelector("#page-wrap").classList.add("scroll-lock");
     }
 
     if (trigger.hasAttribute("close")) {
       modal.close();
-      document.querySelector("body").classList.remove("scroll-lock");
+      document.querySelector("#page-wrap").classList.remove("scroll-lock");
     }
   });
 });
